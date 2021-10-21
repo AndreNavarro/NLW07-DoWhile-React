e581266435c3e525ad67c6d924727702cfd2dfe3
AndreNavarro <andre-navarro@hotmail.com>
2021-10-20 22:10:46 -0300

    :tada: Projeto React - Do While - Tela inicial, componentes e estilização - #NLW07

---
+++ src/components/MessageList/index.tsx
@@ -0,0 +1,54 @@
+import  { api } from '../../services/api';
+
+import styles from './styles.module.scss';
+
+import logoImg from '../../assets/logo.svg';
+import { useEffect } from 'react';
+
+export function MessageList(){
+
+  useEffect(() => {
+    api.get('messages/last3').then(response => {
+      console.log(response.data);
+    })
+  }, [])
+
+
+  return(
+    <div className={styles.messageListWrapper}>
+      <img src={logoImg} alt="DoWhile 2021" />
+
+      <ul className={styles.messageList}>
+        <li className={styles.message}>
+          <p className={styles.messageContent}>Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos os tempos, vamooo pra cima! 🔥🔥</p>
+          <div className={styles.messageUser}>
+            <div className={styles.userImage}>
+              <img src="https://github.com/AndreNavarro.png" alt="André Navarro" />
+            </div>
+            <span>André Navarro</span>
+          </div>
+        </li>
+        <li className={styles.message}>
+          <p className={styles.messageContent}>Esse vai ser simplesmente fantástico! Bora aprender tudo em relação a montagem de APIs GraphQL. Sem contar com as palestras e painéis.</p>
+          <div className={styles.messageUser}>
+            <div className={styles.userImage}>
+              <img src="https://github.com/AndreNavarro.png" alt="André Navarro" />
+            </div>
+            <span>André Navarro</span>
+          </div>
+        </li>
+        <li className={styles.message}>
+          <p className={styles.messageContent}>Sem dúvida as palestras vão ser úteis para a minha carreira e para a de muitos 😍 #gorocket</p>
+          <div className={styles.messageUser}>
+            <div className={styles.userImage}>
+              <img src="https://github.com/AndreNavarro.png" alt="André Navarro" />
+            </div>
+            <span>André Navarro</span>
+          </div>
+        </li>
+
+
+      </ul>
+    </div>
+  )
+}
